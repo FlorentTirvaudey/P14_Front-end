0862ece3e953634c225ec800a3bbda8db3267af3
FlorentTirvaudey <florent.tirvaudey@gmail.com>
2024-10-08 17:09:42 +0200

    initialize project with react

---
+++ src/pages/EmployeeList.jsx
@@ -0,0 +1,13 @@
+function EmployeeList() {
+    return (
+        <>
+            <div id="employee-div" className="container">
+                <h1>Current Employees</h1>
+                <table id="employee-table" className="display"></table>
+                <a href="index.html">Home</a>
+            </div>
+        </>
+    )
+};
+
+export default EmployeeList;
